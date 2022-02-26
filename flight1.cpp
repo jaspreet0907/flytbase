3c2948e9022170125bc6d08df3b623d9118cd3d2
jaspreet0907 <100332099+jaspreet0907@users.noreply.github.com>
2022-02-26 12:20:19 +0530

    Update flight1.cpp

--- flight1.cpp
+++ flight1.cpp
@@ -3,10 +3,10 @@
 Navigation nav;
 int main(int argc, char *argv[]){
     nav.take_off(5.0); // drone take off 
-    nav.position_set(6.5,0,-5); //first position setpoint
-    nav.position_set(6.5,6.5,-5); //second position setpoint
-    nav.position_set(0,6.5,-5); // third position setpoint
-    nav.position_set(0,0,-5); //fourth position setpoint
+    nav.position_set(6.5,0,0); //first position setpoint
+    nav.position_set(0,6.5,0); //second position setpoint
+    nav.position_set(-6.5,0,0); // third position setpoint
+    nav.position_set(0,-6.5,0); //fourth position setpoint
     nav.land(false); //Land
 }
 
